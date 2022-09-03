c0c2f72aebf2e78bef99941670cff433d9451100
Taeryn Denhertog <taeryn@hotmail.ca>
2022-09-03 11:22:21 -0400

    fixed instantiating slimes and respawning on player

--- Newbies Game Jam 2022 - GG/Assets/Scripts/SlimePossession.cs
+++ Newbies Game Jam 2022 - GG/Assets/Scripts/SlimePossession.cs
@@ -5,44 +5,38 @@ using UnityEngine.Events;
 
 public class SlimePossession : MonoBehaviour
 {
-    /*public GameObject ghost;
-    public GameObject slime;
-
-    public PlayerMovement movement;
-
-    public Camera slimeCamera;
-
-    private void OnTriggerEnter2D(Collider2D col)
-    {
-        if (col.tag == "Ghost")
-        {
-            Destroy(col.gameObject);
-            slimeCamera.enabled = true;
-            movement.enabled = true;
-            Debug.Log("Touching");
-        }
-        else if (col.tag == "Player")
-        {
-            Destroy(gameObject);
-            slimeCamera.enabled = false;
-            Instantiate(slime, col.transform.position + new Vector3(0, 2, 0), Quaternion.identity);
-
-            Instantiate(ghost, col.transform.position + new Vector3(3, 0, 0), Quaternion.identity);
-        }
-    }*/ //Old Broken Code
-
     public GameObject slime;
     public GameObject player;
+
+    public GameObject[] spawnLocations;
+
+    public SpriteRenderer sprRend;
+    public Sprite slimeModel;
+    public Sprite ghostModel;
+
+    void Awake()
+    {
+        spawnLocations = GameObject.FindGameObjectsWithTag("Spawnpoint");
+    }
 
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.tag == "Slime")
         {
-            //SpawnListener.summon += Instantiate();
+            Destroy(col.gameObject);
+
+            sprRend.sprite = slimeModel;
+        }
+        else if (col.tag == "Player" && sprRend.sprite == slimeModel)
+        {
+            sprRend.sprite = ghostModel;
+            Instantiate();
         }
     }
+
     void Instantiate()
     {
-        Instantiate(slime, new Vector3(2, 2, 0), Quaternion.identity);
+        int spawn = Random.Range(0, spawnLocations.Length);
+        Instantiate(slime, spawnLocations[spawn].transform.position, Quaternion.identity);
     }
 }
